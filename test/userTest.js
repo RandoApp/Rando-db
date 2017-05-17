735b6542cdd0626a8b01f29540db7180547055f0
dimhold <dimhold@gmail.com>
2017-05-17 17:56:54 +0300

    Implement methods for report

--- test/userTest.js
+++ test/userTest.js
@@ -2,6 +2,7 @@ var sinon = require("sinon");
 var config = require("config");
 var db = require("../lib/randoDB");
 var should = require("should");
+var async = require("async");
 
 describe("db.user.", function() {
   before(function(done) {
@@ -367,4 +368,159 @@ describe("db.user.", function() {
       });
     });
   });
+
+  describe("updateReportFlagForInRando. ", function() {
+    beforeEach(function(done) {
+      var user = {
+        email: "user@rando4.me",
+        report: [],
+        in: [{
+          randoId: 1,
+          report: 0,
+          delete: 0
+        }, {
+          randoId: 2,
+          report: 0,
+          delete: 0
+        }, {
+          randoId: 3,
+          report: 1,
+          delete: 1
+        }],
+        out: [{
+          randoId: 4,
+          report: 0,
+          delete: 0
+        }]
+      };
+      
+      db.user.create(user, done);
+    });
+
+    it("Should update report to 1 for in rando by email and randoId", (done) => {
+      db.user.updateReportFlagForInRando("user@rando4.me", 2, 1, (err) => {
+        should.not.exist(err);
+        db.user.getByEmailLight("user@rando4.me", (err, user) => {
+          should.not.exist(err);
+          should.exist(user);
+          user.in.should.have.length(3);
+          user.in[0].should.have.properties({randoId: "1", report: 0, delete: 0});
+          user.in[1].should.have.properties({randoId: "2", report: 1, delete: 0});
+          user.in[2].should.have.properties({randoId: "3", report: 1, delete: 1});
+          done();
+        });
+      });
+    });
+
+    it("Should update report to 0 for in rando by email and randoId", (done) => {
+      db.user.updateReportFlagForInRando("user@rando4.me", 3, 0, (err) => {
+        should.not.exist(err);
+        db.user.getByEmailLight("user@rando4.me", (err, user) => {
+          should.not.exist(err);
+          should.exist(user);
+          user.in.should.have.length(3);
+          user.in[0].should.have.properties({randoId: "1", report: 0, delete: 0});
+          user.in[1].should.have.properties({randoId: "2", report: 0, delete: 0});
+          user.in[2].should.have.properties({randoId: "3", report: 0, delete: 1});
+          done();
+        });
+      });
+    });
+  });
+
+  describe("addReportForUser. ", function() {
+    beforeEach(function(done) {
+      var user = {
+        email: "user@rando4.me",
+        report: [],
+        in: [{
+          randoId: 1,
+          report: 0,
+          delete: 0
+        }, {
+          randoId: 2,
+          report: 0,
+          delete: 0
+        }, {
+          randoId: 3,
+          report: 1,
+          delete: 1
+        }],
+        out: [{
+          randoId: 4,
+          report: 0,
+          delete: 0
+        }]
+      };
+      
+      db.user.create(user, done);
+    });
+
+    it("Should add report data by email", (done) => {
+      var now = Date.now();
+      var reportData = {
+        reportedBy: "user1@rando4.me",
+        reason: "Reported by user1@rando4.me because randoId: 3",
+        reportedDate: now,
+        randoId: "2"
+      };
+
+      db.user.addReportForUser("user@rando4.me", reportData, (err) => {
+        should.not.exist(err);
+        db.user.getByEmailLight("user@rando4.me", (err, user) => {
+          should.not.exist(err);
+          should.exist(user);
+          user.report.should.have.length(1);
+          user.report[0].should.have.properties({
+            reportedBy: "user1@rando4.me",
+            reason: "Reported by user1@rando4.me because randoId: 3",
+            reportedDate: now,
+            randoId: "2"
+          });
+          done();
+        });
+      });
+    });
+
+    it("Should add report a lot of data by email", (done) => {
+      var now = Date.now();
+      var reportData = [{
+        reportedBy: "user1@rando4.me",
+        reason: "Reported by user1@rando4.me because randoId: 3",
+        reportedDate: now,
+        randoId: "2"
+      }, {
+        reportedBy: "user99@rando4.me",
+        reason: "Reported by user99@rando4.me because randoId: 99",
+        reportedDate: now,
+        randoId: "99"
+      }];
+
+      async.eachLimit(reportData, 1, (data, parallelDone) => {
+          db.user.addReportForUser("user@rando4.me", data, parallelDone);
+      }, (err) => {
+        should.not.exist(err);
+        db.user.getByEmailLight("user@rando4.me", (err, user) => {
+          should.not.exist(err);
+          should.exist(user);
+          user.report.should.have.length(2);
+          user.report[0].should.have.properties({
+            reportedBy: "user1@rando4.me",
+            reason: "Reported by user1@rando4.me because randoId: 3",
+            reportedDate: now,
+            randoId: "2"
+          });
+          user.report[1].should.have.properties({
+            reportedBy: "user99@rando4.me",
+            reason: "Reported by user99@rando4.me because randoId: 99",
+            reportedDate: now,
+            randoId: "99"
+          });
+          done();
+        });
+      });
+    });
+
+  });
+
 });
