fbbd3f2e8d53aa6a1f7831cfeaf51a94a180932b
Viktar Patotski <xp.vit.blr@gmail.com>
2017-06-01 12:21:00 +0300

    add DB connext/disconnect tests

---
+++ test/randoDBTest.js
@@ -0,0 +1,20 @@
+var sinon = require("sinon");
+var config = require("config");
+var db = require("../lib/randoDB");
+var should = require("should");
+var async = require("async");
+
+describe("randoDB.", () => {
+	it("Should connect successfully and callback without error and disconnect", (done) => {
+		 db.connect(config.test.db.url,(err)=> {
+		 	should.not.exist(err);
+		 	db.disconnect(done);
+		 });
+	});
+	it("Should Not connect and callback with error when bad url passed", (done) => {
+		 db.connect("NOT_A_MONGO_URL",(err)=> {
+		 	should.exist(err);
+		 	done();
+		 });
+	});
+});
