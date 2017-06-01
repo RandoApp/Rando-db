7f64bb72490322e9d0cd52f35a2331369199d261
Viktar Patotski <xp.vit.blr@gmail.com>
2017-06-01 14:05:23 +0300

    add query to test

--- test/randoDBTest.js
+++ test/randoDBTest.js
@@ -9,16 +9,20 @@ describe("randoDB.", () => {
 		db.disconnect(done);
 	});
 
-	/*it("Should connect successfully and callback without error and disconnect", (done) => {
+	it("Should connect successfully and callback without error and disconnect", (done) => {
 		 db.connect(config.test.db.url,(err)=> {
 		 	should.not.exist(err);
+		 	 db.user.getAll(function(err, users) {
+          	should.not.exist(err);
+          	users.should.have.length(0);
 		 	db.disconnect(done);
 		 });
-	});*/
-	it("Should Not connect and callback with error when bad url passed", (done) => {
+		 });
+	});
+	/*it("Should Not connect and callback with error when bad url passed", (done) => {
 		 db.connect("NOT_A_MONGO_URL",(err)=> {
 		 	should.exist(err);
 		 	db.disconnect(done);
 		 });
-	});
+	});*/
 });
